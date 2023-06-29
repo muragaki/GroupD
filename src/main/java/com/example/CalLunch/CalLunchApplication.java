465b865cdbb13fb1c248fe0bafc20b85bcce47e2
3030668 <3030668@30306-68>
2023-06-29 09:18:26 +0900

    1commit

---
+++ src/main/java/com/example/CalLunch/CalLunchApplication.java
@@ -0,0 +1,13 @@
+package com.example.CalLunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalLunchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalLunchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CalLunch/CalLunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CalLunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalLunchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
