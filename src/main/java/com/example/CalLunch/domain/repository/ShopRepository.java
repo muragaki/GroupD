5d7e2b5d1d6aa5c87c1a83919e636b8b2cc94df4
3030707 <3030707@30307-07>
2023-07-05 09:21:59 +0900

    shopRepository

---
+++ src/main/java/com/example/CalLunch/domain/repository/ShopRepository.java
@@ -0,0 +1,13 @@
+package com.example.CalLunch.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.CalLunch.domain.model.Shop;
+
+public interface ShopRepository extends JpaRepository<Shop, Integer>{
+	List<Shop> findAllById (Integer shopId);
+	Shop readByItemcode(String shopName);
+	Shop readByItemcodeAndShopId(String shopName, Integer shopId);
+}
