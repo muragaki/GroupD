de410f2ecbc0ef03acb85edc484d306f6a28ecb6
3030820 <3030820@30308-20>
2023-07-05 10:07:11 +0900

    あああ

---
+++ src/main/java/com/example/CalLunch/domain/service/comment/CommentService.java
@@ -0,0 +1,18 @@
+package com.example.CalLunch.domain.service.comment;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.CalLunch.domain.model.Comment;
+import com.example.CalLunch.domain.repository.comment.CommentRepository;
+
+@Service
+public class CommentService {
+	@Autowired
+	CommentRepository commentRepository;
+	
+	public void save(Comment comment) {
+		commentRepository.save(comment);
+	}
+
+}
