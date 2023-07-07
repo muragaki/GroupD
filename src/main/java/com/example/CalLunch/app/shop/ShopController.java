e708d1f6b59d1dfc8c984fa1237a9d406d95cdcd
Yokogawa <hoiminnbesu@yahoo.co.jp>
2023-07-07 11:09:51 +0900

    Merge branch 'master' of https://github.com/muragaki/GroupD.git

--- src/main/java/com/example/CalLunch/app/shop/ShopController.java
+++ src/main/java/com/example/CalLunch/app/shop/ShopController.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.CalLunch.domain.model.Cooking;
 import com.example.CalLunch.domain.model.Shop;
+import com.example.CalLunch.domain.service.cooking.CookingService;
 import com.example.CalLunch.domain.service.shop.ShopService;
 
 @Controller
@@ -15,6 +18,9 @@ public class ShopController {
 	@Autowired
 	ShopService shopService;
 	Shop shop = new Shop();
+	
+	@Autowired
+	CookingService cookingService; 
 	
 	@GetMapping("detail")
 	public String detail(Model model) {
@@ -39,14 +45,25 @@ public class ShopController {
 		return "register/register";
 	}
 	
-	@PostMapping("confirmation")
-	public String confirmation(Model model) {
-		return "";
-	}
 	
 	@PostMapping("serch")
 	public String sercher(Model model) {
 		model.addAttribute("serch", shopService.findShop());
 		return "serch/cooking";
 	}
+	@PostMapping("confirmation")
+	public String confirmation(Model model) {
+		model.addAttribute("item", shopService.findShop());
+		model.addAttribute("cook", cookingService.findCooking());
+		return "register/preview";
+	}
+	@PostMapping("/preview")
+    public String preview(@RequestParam("shopList") Shop shop, @RequestParam("cookingList") Cooking cooking) {
+        // shopListとcookingListのデータを使用して処理を行う
+        // 例: データの表示、保存、操作など
+		shopService.save(shop);
+		cookingService.save(cooking);
+
+        return ""; // 表示するビューの名前を返す
+    }
 }
